0617b2dce673b05163e92e17293c22947d5f3d2b
garvinks <144575241+garvinks@users.noreply.github.com>
2024-02-19 15:44:31 +0800

    feat: init web

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ Description: This script is used to do something.
 import os
 import sys
 
+import asyncio
+import tornado
+
 BASE_PATH = os.path.dirname(__file__)
 sys.path.append(BASE_PATH)
 
@@ -21,10 +24,24 @@ logger_info = logger_split(log_name="web_biz_server.log")
 logger_error = logger_common(log_name="error.log")
 
 
-def main():
-    logger_info.info("main")
-    logger_error.info("main")
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        logger_info.info("main")
+        logger_error.info("main")
+        self.write("Hello, world")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+async def main():
+    app = make_app()
+    app.listen(8888)
+    await asyncio.Event().wait()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())

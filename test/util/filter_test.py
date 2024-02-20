82ded611c1ed8e700ccf6c13d85091d38b24618c
garvinks <144575241+garvinks@users.noreply.github.com>
2024-02-20 09:14:07 +0800

    fix: 优化代码结构

--- test/util/filter_test.py
+++ test/util/filter_test.py
@@ -6,13 +6,13 @@ import time
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(BASE_PATH)
 
-from service.filter import class_filter
+from util.filter_util import class_filter_util
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         for i in range(6):
-            class_filter.filter_ip("123.1.1.1")
+            class_filter_util.filter_by_str("123.1.1.1")
             time.sleep(0.5)
         self.assertEqual(True, True)  # add assertion here
 
--- util/filter_util.py
+++ util/filter_util.py
@@ -22,27 +22,27 @@ logger_info = logger_split(log_name="web_biz_server.log")
 logger_error = logger_common(log_name="error.log")
 
 
-class Filter(object):
+class FilterUtil(object):
     def __init__(self):
         self.map = dict()
         self.key_ip = "ip"
         self.map[self.key_ip] = dict()
 
-    def filter_ip(self, ip: str) -> bool:
+    def filter_by_str(self, s: str) -> bool:
         current_time = int(time.time())
-        val_ip = self.map.get(self.key_ip, dict()).get(ip, list())
+        val_ip = self.map.get(self.key_ip, dict()).get(s, list())
         in_time_ip = []
         for i in val_ip:
             if current_time - i > 60:
                 continue
             in_time_ip.append(i)
         if len(in_time_ip) > 10:
-            logger_error.debug(f"error - {ip} - {in_time_ip}")
+            logger_error.debug(f"error - {s} - {in_time_ip}")
             return False
         in_time_ip.append(current_time)
-        self.map[self.key_ip][ip] = in_time_ip
-        logger_info.debug(f"success - {ip} - {in_time_ip}")
+        self.map[self.key_ip][s] = in_time_ip
+        logger_info.debug(f"success - {s} - {in_time_ip}")
         return True
 
 
-class_filter = Filter()
+class_filter_util = FilterUtil()
